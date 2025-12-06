09217bf9d53450cf662234709188a7ec53e5dd14
Sandhya <sandhyadayanithi@gmail.com>
2025-12-06 21:06:27 +0530

    Problem-4 solved

---
+++ December - 04/p4.py
@@ -0,0 +1,24 @@
+def subArray(input,n,target):
+  sum=0
+  start,end=0,0
+  found=0
+  for i in range(n):
+    if sum<target:
+      sum+=input[i]
+      end+=1
+    while sum>target:
+      sum-=input[start]
+      start+=1
+    if sum==target:
+      found=1
+
+  if found:
+    print(f'{start, end}')
+  else:
+    print((-1, -1))
+
+n=7
+target=15
+input=[1,2,3,7,5,1,2]
+
+subArray(input,n,target)
